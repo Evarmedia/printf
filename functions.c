7aaa3a6fdf77b2c80e9eb8a5e62609d4209b9900
Evarmedia <mishakmanuel@gmail.com>
2023-07-25 16:34:16 +0000

    update

--- functions.c
+++ functions.c
@@ -3,8 +3,8 @@
 /************************* PRINT CHAR *************************/
 
 /**
- * print_char - Prints a char
- * @types: List a of arguments
+ * print_char - Prints character
+ * @types: List of argss
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: Width
